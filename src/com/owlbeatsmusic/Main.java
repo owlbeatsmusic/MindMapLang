58c7d32395876cdd762f918ba9c8c237ffeb5c5c
Allan <swedengt06@gmail.com>
2023-03-06 17:55:08 +0100

    test commit

--- src/com/owlbeatsmusic/Main.java
+++ src/com/owlbeatsmusic/Main.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class Main {
 
-    // .mml - mindmaplang
+    // .mml - mindmaplang hejsan
 
     static class Item {
         ArrayList<Integer> connections = new ArrayList<>();
